2fafef01ae64c7e429eed863564653a4264d8d43
Bharath Chinnam <chinnambhrt@gmail.com>
2021-06-03 21:23:31 -0400

    change routing

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
 import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-weather-card',
-  templateUrl:'./weather-card.component.html',
+  templateUrl: './weather-card.component.html',
   styles: [
   ]
 })
@@ -13,29 +14,35 @@ export class WeatherCardComponent implements OnInit {
   @Input("zip") zipCode?: any;
   @Input("index") index: number = -1;
   @Input("delete") delete?: any;
-  weather?: any = {weather:[{main: ""}],main: ""};
+  weather?: any = { weather: [{ main: "" }], main: "" };
   imageSrc?: string;
 
   constructor(
     private storage: StorageService,
-    private weatherService: WeatherService
-    ) { }
+    private weatherService: WeatherService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.weatherService.getCurrentWeather(this.zipCode).subscribe(response=>{
+    this.weatherService.getCurrentWeather(this.zipCode).subscribe(response => {
       this.weather = response;
       let env: string = this.weather.weather[0].main;
       this.imageSrc = this.weatherService.getImageSources()[env];
-    },err=>{
-      if(404==err.status){
+    }, err => {
+      if (404 == err.status) {
         this.deleteZip();
       }
     });
-
   }
 
-  deleteZip(){
+  deleteZip() {
     this.storage.removeLocation(this.zipCode);
   }
+
+
+  showForecast() {
+    this.router.navigate([`forecast/${this.zipCode}`]);
+  }
+
 
 }
